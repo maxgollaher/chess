d3dd72f4b1cbb78cc2244015035a7ffe1fa3f560
maxgollaher <max.c.gollaher@gmail.com>
2023-10-16 01:25:39 -0600

    javadoc zip

--- javadocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"chess.dataAccess"},{"l":"chess.models"},{"l":"chess.services"},{"l":"chess.services.requests"},{"l":"chess.services.responses"}];updateSearchResults();
