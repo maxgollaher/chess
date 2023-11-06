6076c90b52d3292acea512c290cec6f9c0ed40fa
maxgollaher <max.c.gollaher@gmail.com>
2023-11-06 10:38:25 -0700

    2/3 tests

--- src/test/daoTests/AuthTokenDaoTest.java
+++ src/test/daoTests/AuthTokenDaoTest.java
@@ -1,8 +1,15 @@
 package daoTests;
 
 import dataAccess.AuthTokenDao;
+import dataAccess.DataAccessException;
+import dataAccess.Database;
+import models.AuthToken;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
 
 public class AuthTokenDaoTest {
 
@@ -18,10 +25,107 @@ public class AuthTokenDaoTest {
 
     @BeforeEach
     void setup() {
-        // Make sure that the database is clear before each test
+        Assertions.assertDoesNotThrow(authTokenDao::clear);
+    }
+
+    @Test
+    @DisplayName("Test insert authToken")
+    void testInsert() {
+        // test inserting a new authToken
+        AuthToken testAuthToken = new AuthToken("testAuth", "username");
+        Assertions.assertDoesNotThrow(() -> authTokenDao.insert(testAuthToken));
+
+        // see if the authToken was inserted
+        var foundToken = Assertions.assertDoesNotThrow(() -> authTokenDao.find("testAuth"));
+        Assertions.assertEquals(testAuthToken, foundToken);
+    }
+
+    @Test
+    @DisplayName("Test insert authToken fail")
+    void testInsertFail() {
+        // test insert
+        AuthToken testAuthToken = new AuthToken("testAuth", "username");
+        Assertions.assertDoesNotThrow(() -> authTokenDao.insert(testAuthToken));
+
+        // test inserting a duplicate authToken with the same primary key
+        Assertions.assertThrows(DataAccessException.class, () -> authTokenDao.insert(testAuthToken));
+    }
+
+    @Test
+    @DisplayName("Test delete authToken")
+    void testDelete() {
+        // test deleting an authToken
+        AuthToken testAuthToken = new AuthToken("testAuth", "username");
+        Assertions.assertDoesNotThrow(() -> authTokenDao.insert(testAuthToken));
+        Assertions.assertDoesNotThrow(() -> authTokenDao.delete(testAuthToken.getAuthToken()));
+
+        // see if the authToken was deleted
+        Assertions.assertNull(Assertions.assertDoesNotThrow(() -> authTokenDao.find("testAuth")));
+    }
+
+    @Test
+    @DisplayName("Test delete authToken fail")
+    void testDeleteFail() {
+        // SQL doesn't throw an error when a nonexistent authToken is deleted
+        // test that deleting a nonexistent authToken doesn't throw an error
+        Assertions.assertDoesNotThrow(() -> authTokenDao.delete("authToken"));
+    }
+
+    @Test
+    @DisplayName("Test find authToken")
+    void testFind() {
+        // test finding an authToken
+        AuthToken testAuthToken = new AuthToken("testAuth", "username");
+        Assertions.assertDoesNotThrow(() -> authTokenDao.insert(testAuthToken));
+
+        var foundToken = Assertions.assertDoesNotThrow(() -> authTokenDao.find("testAuth"));
+        Assertions.assertEquals(testAuthToken, foundToken);
+    }
+
+    @Test
+    @DisplayName("Test find authToken fail")
+    void testFindFail() {
+        // test that finding a nonexistent authToken doesn't throw an error, and that it returns null
+        Assertions.assertNull(Assertions.assertDoesNotThrow(() -> authTokenDao.find("nonexistent")));
+    }
+
+    @Test
+    @DisplayName("Test find all authTokens")
+    void testFindAll() {
+        // insert several tokens
+        AuthToken testAuthToken = new AuthToken("testAuth", "username");
+        AuthToken testAuthToken2 = new AuthToken("testAuth2", "username2");
+        Assertions.assertDoesNotThrow(() -> authTokenDao.insert(testAuthToken));
+        Assertions.assertDoesNotThrow(() -> authTokenDao.insert(testAuthToken2));
+
+        // see if the authTokens were found
+        var foundTokens = Assertions.assertDoesNotThrow(authTokenDao::findAll);
+        Assertions.assertEquals(2, foundTokens.size());
+        Assertions.assertTrue(foundTokens.contains(testAuthToken));
+        Assertions.assertTrue(foundTokens.contains(testAuthToken2));
+    }
+
+    @Test
+    @DisplayName("Test find all authTokens fail")
+    void testFindAllFail() {
+        // test that the result list is empty when there are no authTokens
+        Assertions.assertTrue(Assertions.assertDoesNotThrow(authTokenDao::findAll).isEmpty());
+    }
+
+    @Test
+    @DisplayName("Test clear authTokens")
+    void testClear() {
+        // insert a token, assert that it is in the database
+        AuthToken testAuthToken = new AuthToken("testAuth", "username");
+        Assertions.assertDoesNotThrow(() -> authTokenDao.insert(testAuthToken));
+        Assertions.assertFalse(Assertions.assertDoesNotThrow(authTokenDao::findAll).isEmpty());
+
+        // clear the database, assert that the token is no longer in the database
         Assertions.assertDoesNotThrow(authTokenDao::clear);
         Assertions.assertTrue(Assertions.assertDoesNotThrow(authTokenDao::findAll).isEmpty());
     }
+
+
 
 
 
---
+++ src/test/daoTests/UserDaoTest.java
@@ -0,0 +1,110 @@
+package daoTests;
+
+import dataAccess.UserDao;
+import dataAccess.DataAccessException;
+import models.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserDaoTest {
+
+    private static final UserDao userDao;
+
+    static {
+        try {
+            userDao = new UserDao();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    void setup() {
+        Assertions.assertDoesNotThrow(userDao::clear);
+    }
+
+    @Test
+    @DisplayName("Test insert User")
+    void testInsert() {
+        // test inserting a new User
+        User testUser = new User("username","email","password");
+        Assertions.assertDoesNotThrow(() -> userDao.insert(testUser));
+
+        // see if the User was inserted
+        var foundToken = Assertions.assertDoesNotThrow(() -> userDao.find("username"));
+        Assertions.assertEquals(testUser, foundToken);
+    }
+
+    @Test
+    @DisplayName("Test insert User fail")
+    void testInsertFail() {
+        // test insert
+        User testUser = new User("username","email","password");
+        Assertions.assertDoesNotThrow(() -> userDao.insert(testUser));
+
+        // test inserting a duplicate User with the same primary key
+        Assertions.assertThrows(DataAccessException.class, () -> userDao.insert(testUser));
+    }
+
+    @Test
+    @DisplayName("Test find User")
+    void testFind() {
+        // test finding a User
+        User testUser = new User("username","email","password");
+        Assertions.assertDoesNotThrow(() -> userDao.insert(testUser));
+
+        var foundToken = Assertions.assertDoesNotThrow(() -> userDao.find("username"));
+        Assertions.assertEquals(testUser, foundToken);
+    }
+
+    @Test
+    @DisplayName("Test find Users fail")
+    void testFindFail() {
+        // test that finding a nonexistent User doesn't throw an error, and that it returns null
+        Assertions.assertNull(Assertions.assertDoesNotThrow(() -> userDao.find("nonexistent")));
+    }
+
+    @Test
+    @DisplayName("Test find all Users")
+    void testFindAll() {
+        // insert several Users
+        User testUser = new User("username","email","password");
+        User testUser2 = new User("username2","email2","password2");
+        Assertions.assertDoesNotThrow(() -> userDao.insert(testUser));
+        Assertions.assertDoesNotThrow(() -> userDao.insert(testUser2));
+
+        // see if the Users were found
+        var foundTokens = Assertions.assertDoesNotThrow(userDao::findAll);
+        Assertions.assertEquals(2, foundTokens.size());
+        Assertions.assertTrue(foundTokens.contains(testUser));
+        Assertions.assertTrue(foundTokens.contains(testUser2));
+    }
+
+    @Test
+    @DisplayName("Test find all Users fail")
+    void testFindAllFail() {
+        // test that the result list is empty when there are no Users
+        Assertions.assertTrue(Assertions.assertDoesNotThrow(userDao::findAll).isEmpty());
+    }
+
+    @Test
+    @DisplayName("Test clear Users")
+    void testClear() {
+        // insert a User, assert that it is in the database
+        User testUser = new User("username","email","password");
+        Assertions.assertDoesNotThrow(() -> userDao.insert(testUser));
+        Assertions.assertFalse(Assertions.assertDoesNotThrow(userDao::findAll).isEmpty());
+
+        // clear the database, assert that the User is no longer in the database
+        Assertions.assertDoesNotThrow(userDao::clear);
+        Assertions.assertTrue(Assertions.assertDoesNotThrow(userDao::findAll).isEmpty());
+    }
+
+
+
+
+
+
+}
